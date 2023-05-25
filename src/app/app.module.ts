eed24928eda8e540f202aa45219553a0f1fd3807
Ambrgna <mittens.pritchett@gmail.com>
2023-05-25 08:38:41 -0500

    upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { FormComponent } from './form/form.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FormComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
